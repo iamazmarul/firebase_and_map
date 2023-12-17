c53aa274467274f5ff5a07ae9cb45406bd74838b
Azmarul Islam <imjohnny.dev@gmail.com>
2023-12-17 22:48:01 +0600

    Google Map

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
-import 'package:firebase_and_map/app.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
-
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
   runApp(const MatchApp());
+}
+
+class MatchApp extends StatelessWidget {
+  const MatchApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
 }
